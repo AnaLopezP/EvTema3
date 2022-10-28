0dbcc58dd43539e2095f48fcee1f2bc748f0d4e2
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-28 15:53:47 +0200

    kjk

--- Ejercicio2/ejercicio2.py
+++ Ejercicio2/ejercicio2.py
@@ -13,3 +13,16 @@ def crear_mat(matriz):
             matriz[i][j] = random.randint(0, 20)
     return matriz
 
+#funcion de sarrus iterativa
+def sarrus_it(matriz):
+    element1 = (matriz[0][0] * matriz[1][1] * matriz[2][2]) + (matriz[1][0]*matriz[2][1]*matriz[0][2]) + (matriz[0][1]*matriz[1][2]*matriz[2][0])
+    element2 = (matriz[0][2]*matriz[1][1]*matriz[2][0]) + (matriz[1][2]*matriz[2][1]*matriz[0][0]) + (matriz[0][1]*matriz[1][0]*matriz[2][2])
+    print("EL DETERMINANTE DE ESTA MATRIZ ES:")
+    determinante = element1 - element2
+    return determinante
+
+mat = crear_mat(matriz)
+print(mat)
+det = sarrus_it(mat)
+print(det)
+
