e4f37d4b17d785cdc8fa432f11bf662d9f344e4c
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-28 16:57:12 +0200

    ya ta una cosa de 499

--- Ejercicio3/main.py
+++ Ejercicio3/main.py
@@ -10,10 +10,6 @@ if __name__ == '__main__':
     while dato != '':
         ejercicio3.Lista.insertar(lista, dato)
         dato = input("Ingresa el nombre de la nave (pulsa enter para cerrar la lista): ")
-        '''   if dato == 'Estrella de la Muerte':
-            print("HAS DADO CON LA ESTRELLA DE LA MUERTE")
-        if dato == 'Halcon Milenario':
-            print("HAS DADO CON EL HALCÓN MILENARIO")'''
         info = []
         a = ejercicio3.Vehiculo(dato)
         ejercicio3.Vehiculo.lista(a, info)
