225d58b3394125be06aae7d7cc106b3ab07f98b8
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-29 20:10:10 +0200

    no me funcionaba el visual no es culpa mia

--- ejercicio1.py
+++ ejercicio1.py
@@ -56,7 +56,7 @@ aguja3 = Pila()
 def añadir(num): 
     if num < 75:
         aguja1.apilar(num)
-        print(aguja1.en_cima)
+        print(aguja1.en_cima())
         añadir(num + 1)
     else:
         pass
@@ -88,8 +88,8 @@ def añadir_discoadisco(num1, num2, num3, aguja_inicial, aguja_quequiero, aguja_
             aguja_inicial.desapilar(num1)
             aguja_quesobra.apilar(num1)
 
-'''aguja_prov = aguja1
+aguja_prov = aguja1
 while aguja3.tamanio != aguja_prov.tamanio:
     comparar(aguja1.cima.info, aguja2.cima.info)
-    añadir_discoadisco(aguja1.cima.info, aguja2.cima.info, aguja3.cima.info, aguja1, aguja2, aguja3)'''
+    añadir_discoadisco(aguja1.cima.info, aguja2.cima.info, aguja3.cima.info, aguja1, aguja2, aguja3)
 
