9aa89ce7be7303556a36faa042e61bf70f596ae8
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-30 19:14:04 +0100

    ddd

--- ejercicio4.py
+++ ejercicio4.py
@@ -5,24 +5,26 @@ pol1_conx = []
 def restar_polinomios(pol1, pol2):
     pass
 
-def print_pol(pol, grad, i, lista):
+def print_term(pol, grad, i, lista):
     ing = "x"
     if i < len(pol):
         term = str(pol[i]) + str(ing) + "^" + str(grad)
         lista.append(term)
         i = i + 1
         grad = grad - 1
-        print_pol(pol, grad, i, pol1_conx)
+        print_term(pol, grad, i, pol1_conx)
         return lista
     else:
         pass
-'''def print_completo(term):
-    lista = []
-    lista.append(term)
-    return lista'''
-    
-pol1_conx = print_pol(pol1, 2, 0, pol1_conx)
+
+def polinomio(lista):
+        result = sum(lista)
+        return result
+
+pol1_conx = print_term(pol1, 2, 0, pol1_conx)
 print(pol1_conx)
+pol1_bien = polinomio(pol1_conx)
+print(pol1_bien)
 
 
 
