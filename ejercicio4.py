1dc1436891a415ecfbc400acc51652e1315e7401
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-30 19:42:00 +0100

    rewstrq

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,9 +1,18 @@
+
 pol1 = [3, -4, 5]
 pol2 = [1, 5, -3]
 pol1_conx = []
 
 def restar_polinomios(pol1, pol2):
-    pass
+    pol_nuevo = []
+    if len(pol1) < len(pol2):
+        pass
+    elif len(pol1) > len(pol2):
+        pass
+    else:
+        for i in range(len(pol1)):
+            pol_nuevo.append(pol1[i] - pol2[i])
+    return pol_nuevo
 
 def print_term(pol, grad, i, lista):
     ing = "x"
@@ -17,17 +26,22 @@ def print_term(pol, grad, i, lista):
     else:
         pass
 
-def polinomio(lista):
-    for i in range(len(lista)):
-        lista[i] = int(lista[i])
-        print(lista[i])
-    result = sum(lista)
-    return result
+def polinomio(lista, len):
+    cont = 0
+    if cont < len:
+        resultado = str(lista[cont]) + " + " +str(lista[cont + 1])
+        cont = cont + 1
+        print(resultado)
+    else:
+        resultado = resultado + str(lista[-1])
+        print(resultado)
+    return resultado
 
 pol1_conx = print_term(pol1, 2, 0, pol1_conx)
 print(pol1_conx)
-pol1_bien = polinomio(pol1_conx)
-print(pol1_bien)
+#pol1_bien = polinomio(pol1_conx, 3)
 
+pol3 = restar_polinomios(pol1, pol2)
+print(pol3)
 
 
