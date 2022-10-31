3575b1803e872c62d6dee39204827a099f507699
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-10-31 12:52:45 +0100

    jjj

--- test/test_matrizDet.py
+++ test/test_matrizDet.py
@@ -2,7 +2,7 @@ import unittest
 from ejercicio2 import *
 
 class Test_MatrizesDet(unittest.TestCase):
-    def det_iterativo(self):
+    def test_det_iterativo(self):
         self.assertEqual(sarrus_it(matriz), -6)
 
 if __name__ == '__main__':
--- test/test_starwars.py
+++ test/test_starwars.py
@@ -2,10 +2,10 @@ import unittest
 from ejercicio3 import *
 
 class Test_Starwars(unittest.TestCase):
-    def ordenar_lista(self):
+    def test_ordenar_lista(self):
         self.assertEqual(ordenar_pasajeros(lista), list_ordenada)
 
-    def at(self):
+    def test_at(self):
         self.assertEqual(at(lista, si_at), si_at)
 
 if __name__ == '__main__':
